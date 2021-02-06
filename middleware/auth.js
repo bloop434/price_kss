a523814a06d8ac93eb0c9fd8929634d2d6e862a8
bloop434 <45686910+bloop434@users.noreply.github.com>
2021-02-06 12:53:49 +0100

    KSS prvi komit
    Prvi komit

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+// Provera da li je korisnik ulogovan
+
+module.exports = {
+	ensureAuth: function(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/');
+		}
+	},
+	ensureGuest: function(req, res, next) {
+		if (!req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/dashboard');
+		}
+	}
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Price
+
+> Napravite javne i privatne price
+
+This app uses Node.js/Express/MongoDB with Google OAuth for authentication
+
+## Projekat iz predmeta Klient Server Sistemi
+
+
